4ed6d7099d8cd47d6fecc41e6c0f69183646f46e
pcoriente <pcoriente.desarrollos@gmail.com>
2021-01-17 01:11:55 -0600

    editamos index.js

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
-app.listen(this.port || process.env.PORT || 3000, function () { return console.log('Servidor funcionando...'); });
+app.listen(process.env.PORT || 3000, function () { return console.log('Servidor funcionando...'); });
