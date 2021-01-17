2563c2e8a8c315bb1abbb91c6b5b678dff273df7
pcoriente <pcoriente.desarrollos@gmail.com>
2021-01-17 10:13:53 -0600

    clientes en index.js prueba

--- src/index.js
+++ src/index.js
@@ -12,3 +12,16 @@ app.listen(process.env.PORT || 3000, function () { return console.log('Servidor 
 app.get('/', (req, res) => {
     res.send('Welcome to my API!');
   }); 
+// all customers
+app.get('/customers', (req, res) => {
+  const sql = 'SELECT * FROM clientes';
+
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
