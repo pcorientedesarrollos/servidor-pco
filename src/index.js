b6a7898bc7385f9e796339b6fc0e04557a6c0edd
pcoriente <pcoriente.desarrollos@gmail.com>
2021-01-17 17:57:34 -0600

    index.js cambio

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 "use strict";
+const mysql = require('mysql');
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,13 +8,20 @@ var express_1 = __importDefault(require("express"));
 var app = express_1.default();
 app.listen(process.env.PORT || 3000, function () { return console.log('Servidor funcionando...'); });
 
-
+// MySql
+const connection = mysql.createConnection({
+  host: 'mysql.pcoriente.com.mx',
+  user: 'useroriente',
+  password: 'Oriente65',
+  // database: 'node_mysql_ts',
+  database: 'webpcoriente',
+});
 // Route
 app.get('/', (req, res) => {
     res.send('Welcome to my API!');
   }); 
 // all customers
-app.get('/customers', (req, res) => {
+app.get('/clientes', (req, res) => {
   const sql = 'SELECT * FROM clientes';
 
   connection.query(sql, (error, results) => {
