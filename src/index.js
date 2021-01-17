5508cd5289f2ebb0f85e57eb7113f50132653194
pcoriente <pcoriente.desarrollos@gmail.com>
2021-01-17 01:04:27 -0600

    cambio de puerto index.js

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
-app.listen(3000, function () { return console.log('Servidor funcionando...'); });
+app.listen(this.port || process.env.PORT || 3000, function () { return console.log('Servidor funcionando...'); });
