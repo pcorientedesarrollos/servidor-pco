15a1adca13a434fa2a39dd92873762c82d4af92a
pcoriente <pcoriente.desarrollos@gmail.com>
2021-01-19 15:02:00 -0600

    creacion en tsconfig de dist y build de src

--- src/index.js
+++ src/index.js
@@ -1,35 +1,47 @@
-"use strict";
-const mysql = require('mysql');
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var app = express_1.default();
-app.listen(process.env.PORT || 3000, function () { return console.log('Servidor funcionando...'); });
+// "use strict";
+// const mysql = require('mysql');
+// const routes = require('./routes')
+
+// const bodyParser = require('body-parser');
+
+// const PORT = process.env.PORT || 3050;
+
+// const app = express();
+// var __importDefault = (this && this.__importDefault) || function (mod) {
+//     return (mod && mod.__esModule) ? mod : { "default": mod };
+// };
+// Object.defineProperty(exports, "__esModule", { value: true });
+// var express_1 = __importDefault(require("express"));
+// var app = express_1.default();
 
 // MySql
-const connection = mysql.createConnection({
-  host: 'mysql.pcoriente.com.mx',
-  user: 'useroriente',
-  password: 'Oriente65',
-  // database: 'node_mysql_ts',
-  database: 'webpcoriente',
-});
-// Route
-app.get('/', (req, res) => {
-    res.send('Welcome to my API!');
-  }); 
-// all customers
-app.get('/clientes', (req, res) => {
-  const sql = 'SELECT * FROM clientes';
+// const connection = mysql.createConnection({
+//   host: 'mysql.pcoriente.com.mx',
+//   user: 'useroriente',
+//   password: 'Oriente65',
+//   // database: 'node_mysql_ts',
+//   database: 'webpcoriente',
+// });
 
-  connection.query(sql, (error, results) => {
-    if (error) throw error;
-    if (results.length > 0) {
-      res.json(results);
-    } else {
-      res.send('Not result');
-    }
-  });
-});
+// // Check connect
+// connection.connect(error => {
+//   if (error) throw error;
+//   console.log('Database server running!');
+// });
+// // Route
+// app.get('/', (req, res) => {
+//     res.send('Welcome to my API!');
+  // }); 
+// // all customers
+// app.get('/clientes', (req, res) => {
+//   const sql = 'SELECT * FROM clientes';
+
+//   connection.query(sql, (error, results) => {
+//     if (error) throw error;
+//     if (results.length > 0) {
+//       res.json(results);
+//     } else {
+//       res.send('Not result');
+//     }
+//   });
+// });
