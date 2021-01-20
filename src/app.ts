bb775e52ea504e229e666a17c1220845f5be60ed
pcoriente <pcoriente.desarrollos@gmail.com>
2021-01-20 14:02:32 -0600

    middlewares CORS in app

--- src/app.ts
+++ src/app.ts
@@ -33,6 +33,16 @@ export class App {
         this.app.use(morgan('dev'))
         // this.app.use(express.urlencoded({extended: false})) //recibir datos de formularios
         this.app.use(express.json())
+
+        // Configurar cabeceras y cors
+    this.app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+        next();
+});
+
     }
 
     routes() {
