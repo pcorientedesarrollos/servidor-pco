8a7915b6cbdba232dc8009a082864c4ba0fb7d6f
pcoriente <pcoriente.desarrollos@gmail.com>
2021-02-12 12:59:19 -0600

    se relaciono detalle facturas en servicios

--- src/controllers/servicios.controller.ts
+++ src/controllers/servicios.controller.ts
@@ -7,9 +7,10 @@ export async function getServicios(req: Request,res: Response): Promise<Response
     const fechaFinal = req.params.fin;
     try {
       const conn = await connect();
-      const notas = await conn.query(`SELECT de.*,cli.nombre from equiposcliente ec 
+      const notas = await conn.query(`SELECT de.*,cli.nombre,df.idFactura  from equiposcliente ec 
       LEFT JOIN clientes cli ON cli.id = ec.idCliente
       LEFT JOIN diagnosticosequipos de ON (de.idEquipo = ec.idEquipo) 
+      LEFT JOIN detalle_factura df ON ( df.idDiagnostico = de.id)
       WHERE de.fechaRecepcion BETWEEN '${fechaInicial}' AND '${fechaFinal}'
       ORDER BY de.id DESC`);
       return res.json(notas[0]);
